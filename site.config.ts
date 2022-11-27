149d910ca61880b13f9f4389b6a95c8f02a3f671
createuseraccount <60058050+createuseraccount@users.noreply.github.com>
2022-11-27 15:17:26 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -22,7 +22,7 @@ export default siteConfig({
   linkedin: 'cyberbruharmy',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // cyberbruharmy or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  // youtube: '#', // optional youtube channel name or `channel/cyberbruarmy`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
